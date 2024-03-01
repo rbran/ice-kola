7c2a1589c80f99c602265352c8aebdd3796be1ef
Rubens Brandao <git@rubens.io>
2024-03-01 10:27:53 -0300

    add bindgen for ghidra-decompiler and fix context values

--- ghidra-decompiler/build.rs
+++ ghidra-decompiler/build.rs
@@ -1,118 +1,79 @@
+use std::path::Path;
+
 fn main() {
-    let ghidra_include_path = "../ghidra/Ghidra/Features/Decompiler/src/decompile/cpp";
-    let ghidra_files = DECOMPILER_FILES
-        .iter()
-        .map(|file| format!("{ghidra_include_path}/{file}"));
+    let num_jobs = std::env::var("NUM_JOBS").unwrap();
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let disassembler_dir =
+        format!("{manifest_dir}/../ghidra/Ghidra/Features/Decompiler/src/decompile/cpp");
+
+    let (static_lib, static_lib_name) = match std::env::var("PROFILE").unwrap().as_str() {
+        "debug" => ("libdecomp_dbg.a", "decomp_dbg"),
+        "release" | _ => ("libdecomp.a", "decomp"),
+    };
+
+    // rerun if any file in ghidra change
+    for file in std::fs::read_dir(&disassembler_dir).unwrap() {
+        let file = file.unwrap();
+        let path = file.path();
+        // rerun if any of those files change
+        match file.path().extension().map(|ext| ext.to_str()).flatten() {
+            // code file
+            Some("cc") | Some("hh") | Some("c") | Some("h") | Some("y") | Some("l") => {
+                println!("cargo:rerun-if-changed={}", path.to_str().unwrap())
+            }
+            // Makefile
+            _ if file.file_name().to_str().unwrap() == "Makefile" => {
+                println!("cargo:rerun-if-changed={}", path.to_str().unwrap())
+            }
+            _ => {}
+        }
+    }
+
+    println!("cargo:rerun-if-changed=src/wrapper.cc");
+    println!("cargo:rerun-if-changed=src/wrapper.hh");
+    println!("cargo:rustc-link-search={disassembler_dir}");
+    println!("cargo:rustc-link-lib=static={static_lib_name}");
+
+    // TODO clean the dir first? I think make can't cache the changes by default
+
+    // compile the ghidra decompiler into a static lib
+    assert!(std::process::Command::new("make")
+        .args(["-j", &num_jobs, "-C", &disassembler_dir, static_lib])
+        .status()
+        .unwrap()
+        .success());
+
+    // generate the bindings file
+    let bindings = bindgen::Builder::default()
+        .clang_arg(&format!("-I{disassembler_dir}"))
+        .clang_arg("-std=c++14")
+        //.disable_untagged_union()
+        .enable_cxx_namespaces()
+        .vtable_generation(true)
+        .allowlist_type("ghidra::.*")
+        .blocklist_item("pointer")
+        .blocklist_item("const_pointer")
+        .opaque_type("__gnu_cxx::.*")
+        .opaque_type("std::.*")
+        .header("src/lib.hpp")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = Path::new(&out_dir);
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 
     // Compile and link the C++ code
     cxx_build::bridge("src/lib.rs")
         .flag("-std=c++14")
         .flag("-Wno-unused-parameter")
         .warnings(false)
-        .files(ghidra_files)
-        .file("src/wrapper.cc")
-        .include(ghidra_include_path)
         .include("src")
+        .include(disassembler_dir)
+        .file("src/wrapper.cc")
         .compile("cxxbridge");
-
-    println!("cargo:rerun-if-changed=src/wrapper.cc",);
-    println!("cargo:rerun-if-changed=src/wrapper.hh",);
-    println!("cargo:rerun-if-changed={ghidra_include_path}");
 }
-
-// TODO not all files are required to pcode dump, remove uncessary files to
-// speed up compilation
-const DECOMPILER_FILES: &[&str] = &[
-    "action.cc",
-    "address.cc",
-    "analyzesigs.cc",
-    "architecture.cc",
-    "bfd_arch.cc",
-    "blockaction.cc",
-    "block.cc",
-    "callgraph.cc",
-    "capability.cc",
-    "cast.cc",
-    "codedata.cc",
-    "comment.cc",
-    "condexe.cc",
-    "consolemain.cc",
-    "context.cc",
-    "coreaction.cc",
-    "cover.cc",
-    "cpool.cc",
-    "crc32.cc",
-    "database.cc",
-    "double.cc",
-    "dynamic.cc",
-    "emulate.cc",
-    "emulateutil.cc",
-    "filemanage.cc",
-    "float.cc",
-    "flow.cc",
-    "fspec.cc",
-    "funcdata_block.cc",
-    "funcdata.cc",
-    "funcdata_op.cc",
-    "funcdata_varnode.cc",
-    "globalcontext.cc",
-    "grammar.cc",
-    "graph.cc",
-    "heritage.cc",
-    "ifacedecomp.cc",
-    "ifaceterm.cc",
-    "inject_sleigh.cc",
-    "interface.cc",
-    "jumptable.cc",
-    "libdecomp.cc",
-    "loadimage_bfd.cc",
-    "loadimage.cc",
-    "loadimage_xml.cc",
-    "marshal.cc",
-    "memstate.cc",
-    "merge.cc",
-    "modelrules.cc",
-    "opbehavior.cc",
-    "op.cc",
-    "opcodes.cc",
-    "options.cc",
-    "override.cc",
-    "paramid.cc",
-    "pcodecompile.cc",
-    "pcodeinject.cc",
-    "pcodeparse.cc",
-    "pcoderaw.cc",
-    "prefersplit.cc",
-    "prettyprint.cc",
-    "printc.cc",
-    "printjava.cc",
-    "printlanguage.cc",
-    "rangeutil.cc",
-    "raw_arch.cc",
-    "ruleaction.cc",
-    "rulecompile.cc",
-    "semantics.cc",
-    "signature.cc",
-    "sleigh_arch.cc",
-    "sleighbase.cc",
-    "sleigh.cc",
-    "slghpatexpress.cc",
-    "slghpattern.cc",
-    "slghsymbol.cc",
-    "space.cc",
-    "stringmanage.cc",
-    "subflow.cc",
-    "testfunction.cc",
-    "transform.cc",
-    "translate.cc",
-    "type.cc",
-    "typeop.cc",
-    "unify.cc",
-    "unionresolve.cc",
-    "userop.cc",
-    "variable.cc",
-    "varmap.cc",
-    "varnode.cc",
-    "xml_arch.cc",
-    "xml.cc",
-];
--- ghidra-decompiler/src/wrapper.cc
+++ ghidra-decompiler/src/wrapper.cc
@@ -1,10 +1,7 @@
 #include <cstdint>
-
-#include <string.h>
-
-#include <iostream>
-
 #include <exception>
+#include <iostream>
+#include <string.h>
 
 #include "ghidra-decompiler/src/lib.rs.h"
 #include "wrapper.hh"
@@ -21,18 +18,6 @@ void MyLoadImage::loadFill(uint1 *ptr, int4 size, const Address &addr) {
 // -------------------------------
 //
 // These are the classes/routines relevant to printing a pcode translation
-
-// Here is a simple class for emitting pcode. We simply dump an appropriate
-// string representation straight to standard out.
-class PcodeRawOut : public PcodeEmit {
-  std::ostringstream pcodeStream;
-
-public:
-  virtual void dump(const Address &addr, OpCode opc, VarnodeData *outvar,
-                    VarnodeData *vars, int4 isize);
-  std::string getPcode() const { return pcodeStream.str(); }
-};
-
 static void print_vardata(ostream &s, VarnodeData &data)
 
 {
@@ -41,21 +26,29 @@ static void print_vardata(ostream &s, VarnodeData &data)
   s << ',' << dec << data.size << ')';
 }
 
-void PcodeRawOut::dump(const Address &addr, OpCode opc, VarnodeData *outvar,
-                       VarnodeData *vars, int4 isize) {
-  // Write to pcodeStream instead of cout
-  if (outvar != nullptr) {
-    print_vardata(pcodeStream, *outvar);
-    pcodeStream << " = ";
+// Here is a simple class for emitting pcode. We simply dump an appropriate
+// string representation straight to standard out.
+class PcodeRawOut : public PcodeEmit {
+  std::ostringstream pcodeStream;
+
+public:
+  void dump(const Address &addr, OpCode opc, VarnodeData *outvar,
+            VarnodeData *vars, int4 isize) {
+    // Write to pcodeStream instead of cout
+    if (outvar != nullptr) {
+      print_vardata(pcodeStream, *outvar);
+      pcodeStream << " = ";
+    }
+    pcodeStream << get_opname(opc);
+    // Possibly check for a code reference or a space reference
+    for (int4 i = 0; i < isize; ++i) {
+      pcodeStream << ' ';
+      print_vardata(pcodeStream, vars[i]);
+    }
+    pcodeStream << '\n';
   }
-  pcodeStream << get_opname(opc);
-  // Possibly check for a code reference or a space reference
-  for (int4 i = 0; i < isize; ++i) {
-    pcodeStream << ' ';
-    print_vardata(pcodeStream, vars[i]);
-  }
-  pcodeStream << '\n';
-}
+  std::string getPcode() const { return pcodeStream.str(); }
+};
 
 // TODO configure a base address or just implement a elf reader instead of
 // using a raw binary
@@ -68,24 +61,28 @@ PcodeDecoder::PcodeDecoder(string &specfile, uint8_t *rust_dec)
 
   // Now that context symbol names are loaded by the translator
   // we can set the default context
-  context.setVariableDefault("longMode", 1); // Enable 64-bit mode
-  context.setVariableDefault("addrsize", 2); // Address size is 64-bit
-  context.setVariableDefault("opsize", 2);   // Operand size is 64-bit
+  // x86_64 64bits: longMode = 1, bit64 = 1, addrsize = 2, opsize = 1
+  // x86_64 32bits: longMode = 0, bit64 = 1, addrsize = 1, opsize = 1
+  // x86 32bits   : longMode = 0, bit64 = 0, addrsize = 1, opsize = 0
+  // x86 16bits   : longMode = 0, bit64 = 0, addrsize = 0, opsize = 0
+  context.setVariableDefault("longMode", 1);
+  context.setVariableDefault("bit64", 1);
+  context.setVariableDefault("addrsize", 2);
+  context.setVariableDefault("opsize", 1);
 };
 
 // -------------------------------------
 //
 // Functions called direclty from rust
 
-rust::String PcodeDecoder::decode_addr(uint64_t addr_in,
-                                       uint64_t *instr_len) const {
+rust::String PcodeDecoder::decode_addr(uint64_t addr_in, uint64_t *instr_len) const {
   Address addr(sleigh.getDefaultCodeSpace(), addr_in);
   PcodeRawOut emit; // Set up the pcode dumper
   int4 length;      // Number of bytes of each machine instruction
 
   try {
     *instr_len = sleigh.oneInstruction(emit, addr); // Translate instruction
-  } catch (const ghidra::LowlevelError &e) {
+  } catch (const LowlevelError &e) {
     throw runtime_error("Error: Disassembly failed due to LowlevelError: " +
                         e.explain);
   } catch (const std::exception &e) {
